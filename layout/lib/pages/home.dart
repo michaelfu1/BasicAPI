199f0cd9f28f83589ebc0868360eb87c1cf7c68d
michaelfu1 <91065687+michaelfu1@users.noreply.github.com>
2021-09-22 01:15:37 +0700

    อัพโหลดโปรเจค Flutter:layout ครั้งแรก

---
+++ layout/lib/pages/home.dart
@@ -0,0 +1,102 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:layout/pages/detail.dart';
+
+class HomePage extends StatefulWidget {
+  //const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "ความรู้เกี่ยวกับคอมพิวเตอร์",
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "หน้าแรก"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "ค้นหา"),
+          BottomNavigationBarItem(icon: Icon(Icons.share), label: "แชร์"),
+        ],
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: FutureBuilder(
+            builder: (context, snapshot) {
+              var data = json.decode(snapshot.data.toString());
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return MyBox(data[index]['title'], data[index]['subtitle'],
+                      data[index]['image_url'], data[index]['detail']);
+                },
+                itemCount: data.length,
+              );
+            },
+            future:
+                DefaultAssetBundle.of(context).loadString('assets/data.json'),
+          )),
+    );
+  }
+
+  Widget MyBox(String title, String subtitle, String image_url, String detail) {
+    var v1, v2, v3, v4;
+    v1 = title;
+    v2 = subtitle;
+    v3 = image_url;
+    v4 = detail;
+
+    return Container(
+        margin: EdgeInsets.only(top: 20),
+        padding: EdgeInsets.all(20),
+        //color: Colors.blue[50],
+        height: 150,
+        decoration: BoxDecoration(
+            //color: Colors.blue[50],
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+                image: NetworkImage(image_url),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.50), BlendMode.darken))),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              subtitle,
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            TextButton(
+                onPressed: () {
+                  print("Next Page >>>");
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => DetailPage(v1, v2, v3, v4)));
+                },
+                child: Text("อ่านต่อ"))
+          ],
+        ));
+  }
+}
